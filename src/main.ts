360730e660782f38b22800c4757cc4df6f1ae3ac
Gandrathi Sai Ritheesh <sai.ritheesh@medibuddy.in>
2026-01-06 01:06:49 +0530

    Added Singleton pattern

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import express from "express";
+import { SingleTon1, SingleTon2 }  from "./DesignPatterns/singletonPattern";
+
+
+ const obj1: SingleTon1 = SingleTon1.getSingleTonInstance();
+ const obj2: SingleTon1 = SingleTon1.getSingleTonInstance();
+
+ console.log(obj1 === obj2); // WILL PRINT TRUE
 
 const app = express();
 app.use(express.json());
